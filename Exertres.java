2a0ab81c5450046e74b619f42850bf3be90e9958
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-16 20:31:49 -0300

    Novo exercicio
    estou poupando tempo ao copiar e colar do arquivo anterior e ai vou trocando as informacões

--- Exertres.java
+++ Exertres.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Exertres {
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+        System.out.print("Digite a media(0-10): ");
+        double media = leia.nextDouble();
+
+        if (media >= 7) {
+        
+        System.out.println("Aprovado");
+       } else if (media >= 5) {
+
+         System.out.println("Recuperação");
+       } else {
+          System.out.println("Reprovado");
     
+       
+
+        
+ 
+
+       }
+
+    }
 }
