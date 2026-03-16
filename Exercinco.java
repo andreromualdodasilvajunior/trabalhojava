85e797302151adb58f10adbdc4222e39eda7c4d0
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-16 20:59:36 -0300

    prototipo do exercio 5

---
+++ Exercinco.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Exercinco{
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+        System.out.print("Digite o numero 1: ");
+        double a = leia.nextDouble();
+
+         System.out.print("Digite o numero 2: ");
+        double b = leia.nextDouble();
+
+         System.out.print("Digite o operador (+ - * /): ");
+        char op = leia.next().charAt(0);
+
+        switch(op) {
+          case '+':
+            System.out.println("Resultado: " + (a + b));
+            break;
+
+            case'':
+
+
+
+
+        if (media >= 7) {
+        
+        System.out.println("Aprovado");
+       } else if (media >= 5) {
+
+         System.out.println("Recuperação");
+       } else {
+          System.out.println("Reprovado");
+    
+       
+
+        
+ 
+
+       }
+
+    }
+}
