f2b18c42f8c82a14f9fcdafea26da3b030f8286f
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-19 09:43:05 -0300

    atualizando

---
+++ Exerquatorze.java
@@ -0,0 +1,21 @@
+    import java.util.Scanner;
+
+    public class Exerquatorze{
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+    System.out.print("Digite o primeiro número: ");
+    int a = sc.nextInt();
+    System.out.print("Digite o segundo número: ");
+    int b = sc.nextInt();
+    System.out.print("Digite o terceiro número: ");
+    int c = sc.nextInt();
+
+    int maior = Math.max(a,
+    Math.max(b, c));
+    System.out.println("Maior numero: " + maior);
+    sc.close();
+        }
+    }
+
+
---
+++ Exerquinze.java
@@ -0,0 +1,24 @@
+    import java.util.Scanner;
+
+        public class Exerquinze{
+            public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+    |String usuarioCorreto = "andre";
+    String senhaCorreta = "1567";
+
+    System.out.print("Digite o usuario: ");
+    String usuario = sc.nextLine();
+
+        System.out.print("Digite a senha: ");
+        String senha = sc.nextLine();
+
+    String resultado = (usuario.equals(usuarioCorreto) && senha.equals(senhaCorreta))
+    ? "Acesso permitido"
+    : "Acesso negado";
+    import java.util.Scanner;
+
+        System.out.println(resultado);
+        sc.close();
+            }
+        }
