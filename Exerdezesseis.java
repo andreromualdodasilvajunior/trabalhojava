76d936f89cea8e326bbe046d01c1ae7fe9ce9ca9
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-24 14:44:36 -0300

    novo

--- Exerdezesseis.java
+++ Exerdezesseis.java
@@ -1,7 +1,25 @@
  import java.util.Scanner;
 
-    public class Exerquatorze{
+    public class Exerdezesseis{
         public static void main(String[] args) {
 
             Scanner sc = new Scanner(System.in);
-    System.out.print("Digite o primeiro número: ");
+    System.out.print("Digite o primeiro numero: ");
+        int num = sc.nextInt();
+
+        if (num % 3 == 0 && num % 5 == 0) {
+            System.out.println("Múltiplo de 3 e 5");
+        } else if (num % 3 == 0) {
+            System.out.println("Múltiplo de 3");
+            } else if (num % 5 == 0) {
+                            System.out.println("Múltiplo de 5");
+} else {
+                System.out.println("Nao é multiplo de nenhuma");
+
+}  
+sc.close();
+            }
+        }
+
+        
+       
