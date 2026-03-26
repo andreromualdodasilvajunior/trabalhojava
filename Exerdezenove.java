1dd003e0ce6d6bc8d242453541b8da5a92b7ce41
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-26 09:35:09 -0300

    codig com erros

--- Exerdezenove.java
+++ Exerdezenove.java
@@ -5,12 +5,28 @@ public class Exerdezenove {
 
         Scanner scanner = new Scanner(System.in);
          System.out.print("Digite a velocidade do teu veiculo(km/h): ");
-        double velocidade = sc.nextDouble();
+        double velocidade = scanner.nextDouble();
         
         System.out.print("Digite o limite da via (km/h): ");
-        double limite = sc.nextDouble();
+        double limite = scanner.nextDouble();
  double excesso = velocidade - limite;
          double percentualExcesso = (excesso / limite) * 100; 
-        String classif;
-        if )v
+       
+        String classificacao;
+
+        if (velocidade <= limite);
+    classificacao = "SEM multa";
+     } else if (percentualExcesso <= 20) {
+     classificacao = "Multa leve";
+     } else if (percentualExcesso <= 50) {
+     classificacao = "Multa grave";
+    } else {
+     classificacao = "Multa gravissima + suspensão";
+    }
+ System.out.println("A velocidade é: " + velocidade + " km/h");
+        System.out.println("E o limite era: " + limite + " km/h");
+        System.out.printf("Excesso: %.1f%% acima do limite\n", percentualExcesso);
+        System.out.println("A classificação: " + classificacao);
         
+        scanner.close();
+    }
